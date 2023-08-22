6cb81e3ab2226f114d74aff74cc1d0b27a5d977d
RadoslawKieronski <64900997+RadoslawKieronski@users.noreply.github.com>
2023-08-22 15:55:31 +0200

    Create put.sh
    Signed-off-by: RadoslawKieronski <64900997+RadoslawKieronski@users.noreply.github.com>

---
+++ Ansible/web/put.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+counter=0
+
+while [ $counter -lt 50 ]; do
+  let counter=counter+1
+
+  name=$(nl people.txt | grep -w $counter | awk '{print $2}' | awk -F ',' '{print $1}')
+  lastname=$(nl people.txt | grep -w $counter | awk '{print $2}' | awk -F ',' '{print $2}')
+  age=$(shuf -i 20-25 -n 1)
+  
+  mysql -u root -p 1234 people -e "insert into register values ($counter, '$name', '$lastname', '$age')"
+  echo "$counter, $name, $lastname, $age was correctly imported"
+
+done
+
+# 'nl' - get nb of lines;
+# '{print $2}' - name,lastname;
+# 'awk -F ','' - separate name and lastname;
+# '{print $1}' - show name
+
+# command 'nl people.txt' 
+# output:    1	Denice,Caudle
+
+# 'shuf -i 20-25 -n 1' -  choose values between 20-25, and use the 1st from the output
+
+# 'people' - database
+# 'register' - table
